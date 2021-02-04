6ba0bc1ec02e18be7c083d043f2c10e11f76c401
wjsaveve@163.com <wjsaveve@163.com>
2021-02-04 15:57:53 +0800

    封装UI自动化测试框架-6

--- test_frame/testcase/conftest.py
+++ test_frame/testcase/conftest.py
@@ -10,6 +10,8 @@ import pytest
 def record_vedio():
     cmd = "scrcpy --record file.mp4"
     p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    print(p)
     yield
     os.kill(p.pid, signal.CTRL_C_EVENT)
+    # subprocess.Popen("cmd.exe /k taskkill /F /T /PID %i" % p.pid, shell=True)
+    # print("cmd.exe /k taskkill /F /T /PID:%i" % p.pid)
+    # subprocess.Popen("cmd.exe /k taskkill /F /T /PID %i" % p.pid, shell=True)
