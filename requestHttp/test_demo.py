abe6d018af6a972e234693c9389014236aeb3da2
wjsaveve <wjsaveve@163.com>
2021-03-08 23:32:02 +0800

    接口测试

--- requestHttp/test_demo.py
+++ requestHttp/test_demo.py
@@ -1,9 +1,40 @@
 # -*- coding: utf-8 -*-
 # 创建时间：2021/3/4 21:02
+import pystache
 import requests
+from requests.auth import HTTPBasicAuth
 
 
 class TestDemo:
+    def test_oauth(self):
+        myurl = "https://httpbin.testing-studio.com/basic-auth/banana/123"
+        r = requests.get(url=myurl, auth=HTTPBasicAuth("banana", "123"))
+        print(r.request.headers)
+
+    def test_header_cookie2(self):
+        myurl = "https://httpbin.testing-studio.com/cookies"
+        myheaders = {'User-Agent': 'WJ-requests/2.25.1'}
+        mycookies = dict(name="wangjian")
+        r = requests.get(url=myurl, headers=myheaders, cookies=mycookies)
+        print(r.request.headers)
+
+    def test_header_cookie(self):
+        myurl = "https://httpbin.testing-studio.com/cookies"
+        # Cookie的首字母必须大写
+        myheader = {"Cookie": "name=wangjian"}
+        r = requests.get(url=myurl, headers=myheader)
+        print(r.request.headers)
+
+    def test_ho_json(self):
+        r = requests.get('https://home.testing-studio.com/categories.json')
+        print(r.text)
+
+    def test_pystache(self):
+        pystache.render(
+            'Hi{{person}}!',
+            {'person': 'wangjian'}
+        )
+
     def test_demo(self):
         r = requests.get('https://httpbin.testing-studio.com/get')
         print("cessssssssss")
@@ -51,5 +82,12 @@ class TestDemo:
         print("开始")
         mycookie = dict(cookies_are='working')
         r = requests.get('https://httpbin.testing-studio.com/get', cookies=mycookie)
+        print(r.text)
+        print(r.json())
+
+    def test_json(self):
+        print("开始")
+        payload = {'sonme': 'data'}
+        r = requests.post('https://httpbin.testing-studio.com/psot', json=payload)
         print(r.text)
         print(r.json())
